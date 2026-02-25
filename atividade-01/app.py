0f16df51b6e92dbeea1d49558ed50ef66bf1e37f
lviz-tk <Administrador@PC40.e239.local>
2026-02-25 11:42:22 -0300

    styling added to home page

--- atividade-01/app.py
+++ atividade-01/app.py
@@ -12,11 +12,11 @@ app = Flask(__name__, template_folder='views')
 def home():
     return render_template('index.html')
 
-@app.route('/gays')
+@app.route('/lista')
 def lista():
     return render_template('lista.html')
 
-@app.route('/yaois')
+@app.route('/formulario')
 def formulario():
     return render_template('formulario.html')
 
