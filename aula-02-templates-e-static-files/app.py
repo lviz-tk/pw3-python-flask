e48d0106d9aac0358b12eccfb1919534b30cdc33
luiz <luizcarlosrealgames@gmail.com>
2026-03-09 13:53:54 -0300

    Aula 02 e continuação

---
+++ aula-02-templates-e-static-files/app.py
@@ -0,0 +1,36 @@
+# flask = pacote // Flask = classe
+from flask import Flask, render_template
+
+# declarando variavel do flask como 'app'
+app = Flask(__name__, template_folder='views')
+# app = Flask(app.py)
+# variaveis com __ são variaveis de ambiente do python que ja existem
+# __name__ representa o nome da aplicaçao
+
+# criando rota principal e função (o @ liga as duas)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/games')
+def games():
+    jogo = "Portal 2"
+    ano = "2011"
+    genero = "Puzzle"
+    
+    jogadores = ['Marcos', 'Richard', 'Kirk', 'Epstein', 'Floyd']
+    
+    return render_template('games.html')
+
+@app.route('/consoles')
+def consoles():
+    # criando um objeto
+    console = {"Nome": "Playstation 2", 
+               "Fabricante": "Sony", 
+               "Ano": "2000"}
+    return render_template('consoles.html', console=console)
+
+# iniciar servidor na porta 5000, verificando se o arquivo é o main
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
