cb96232a5fd0887169eea6d0fe3ea95c446f0c54
luiz <luizcarlosrealgames@gmail.com>
2026-03-30 11:35:23 -0300

    routes.py atualizado (copiado de renato lol)

--- aula-03-arquitetura-mvc-e-request/controllers/routes.py
+++ aula-03-arquitetura-mvc-e-request/controllers/routes.py
@@ -1,40 +1,43 @@
-from flask import render_template, request
+
+from flask import Flask, render_template, request, url_for
+
+
+app = Flask(__name__, template_folder='views')
+
 
 def init_app(app):
+
+    listaConsoles = ['Playstation 5', 'Xbox One',
+            'Super Nintendo', 'Atari 2600', 'Nintendo 3DS']
+    listaGames = [{'titulo' : 'CS-GO', 'ano' : 2012, 'categoria' : 'FPS Online', 'plataforma' : 'PC (Windows)'}]
     
     @app.route('/')
-# def cria funções no Python
     def home():
         return render_template('index.html')
 
-
     @app.route('/games')
     def games():
-    # Criando variáveis para a rota de games
+        # Criar informação para a rota de games
         titulo = "Portal 2"
         ano = 2011
         categoria = "Puzzle"
-        # Lista de jogadores (uma lista é um vetor/array)
-        jogadores = ['Marcos', 'Richard', 'Miguel', 'Renato', 'Pedro']
-        # Enviando as variáveis para o HTML
-        return render_template('games.html',
-                            titulo=titulo,
-                            ano=ano,
-                            categoria=categoria,
-                            jogadores=jogadores)
-
+        jogadores = ['Marcos', 'Richard', "Miguel", 'Renato', 'Pedro']
+        return render_template('games.html', titulo=titulo, ano=ano, categoria=categoria, jogadores=jogadores)
 
     @app.route('/consoles', methods=['GET', 'POST'])
     def consoles():
-        # Criando um objetos
-        console = {"Nome": "Playstation 2",
-                "Fabricante": "Sony",
-                "Ano": 2000}    
-        
-        listaConsoles = ['Playstation 5','Xbox One', 'Super Nintendo', 'Atari 3600', '3DS']
-        
+        # criando um objeto
+        console = {"Nome:": "Playstation 2 ",
+                   "Fabricante: ": "Sony", "Ano: ": 2000}
+        # Recebendo o valor do formulário
         if request.method == 'POST':
-            if request.form.get('novoConsole')
-                listaConsoles.append(request.form.get ('novoConsole'))
-        return render_template('consoles.html',
-                            console=console)
+            if request.form.get('novoConsole'):
+                listaConsoles.append(request.form.get('novoConsole'))
+
+        return render_template('consoles.html', console=console, listaConsoles=listaConsoles)
+
+    @app.route('/cadastrar', methods=['GET', 'POST'])
+    def cadastrar():
+        if request.method == 'POST':
+            listaGames.append({'titulo' : request.form.get('titulo'), 'ano' : request.form.get('ano'), 'categoria' : request.form.get('categoria'), 'plataforma' : request.form.get('plataforma')})
+        return render_template('cadastrar.html', listaGames = listaGames)
